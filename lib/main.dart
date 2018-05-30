2976d88c04ca1bd3d8d67ff4c0a6e978ebd89b4b
Matt Carroll <me@mattcarroll.name>
2018-05-30 00:17:25 -0700

    Added card flipping 3D projection.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'dart:ui' show lerpDouble;
 
 import 'package:flip_carousel_proto/card_data.dart';
@@ -145,6 +146,48 @@ class _CardFlipperState extends State<CardFlipper> with TickerProviderStateMixin
     }).toList();
   }
 
+  Matrix4 _buildCardProjection(double scrollPercent) {
+    // Pre-multiplied matrix of a projection matrix and a view matrix.
+    //
+    // Projection matrix is a simplified perspective matrix
+    // http://web.iitd.ac.in/~hegde/cad/lecture/L9_persproj.pdf
+    // in the form of
+    // [[1.0, 0.0, 0.0, 0.0],
+    //  [0.0, 1.0, 0.0, 0.0],
+    //  [0.0, 0.0, 1.0, 0.0],
+    //  [0.0, 0.0, -perspective, 1.0]]
+    //
+    // View matrix is a simplified camera view matrix.
+    // Basically re-scales to keep object at original size at angle = 0 at
+    // any radius in the form of
+    // [[1.0, 0.0, 0.0, 0.0],
+    //  [0.0, 1.0, 0.0, 0.0],
+    //  [0.0, 0.0, 1.0, -radius],
+    //  [0.0, 0.0, 0.0, 1.0]]
+    final perspective = 0.002;
+    final radius = 1.0;
+    final angle = scrollPercent * pi / 8;
+    final horizontalTranslation = 0.0;
+    Matrix4 projection = new Matrix4.identity()
+      ..setEntry(0, 0, 1 / radius)
+      ..setEntry(1, 1, 1 / radius)
+      ..setEntry(3, 2, -perspective)
+      ..setEntry(2, 3, -radius)
+      ..setEntry(3, 3, perspective * radius + 1.0);
+
+    // Model matrix by first translating the object from the origin of the world
+    // by radius in the z axis and then rotating against the world.
+    final rotationPointMultiplier = angle > 0.0 ? angle / angle.abs() : 1.0;
+    print('Angle: $angle');
+    projection *= new Matrix4.translationValues(
+            horizontalTranslation + (rotationPointMultiplier * 300.0), 0.0, 0.0) *
+        new Matrix4.rotationY(angle) *
+        new Matrix4.translationValues(0.0, 0.0, radius) *
+        new Matrix4.translationValues(-rotationPointMultiplier * 300.0, 0.0, 0.0);
+
+    return projection;
+  }
+
   Widget _buildCard(
     CardViewModel viewModel,
     int cardIndex,
@@ -158,9 +201,12 @@ class _CardFlipperState extends State<CardFlipper> with TickerProviderStateMixin
       translation: new Offset(cardIndex - cardScrollPercent, 0.0),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: new Card(
-          viewModel: viewModel,
-          parallaxPercent: parallax,
+        child: new Transform(
+          transform: _buildCardProjection(cardScrollPercent - cardIndex),
+          child: new Card(
+            viewModel: viewModel,
+            parallaxPercent: parallax,
+          ),
         ),
       ),
     );
