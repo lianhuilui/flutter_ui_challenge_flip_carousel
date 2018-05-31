94f073804711ce347e03f3a7e20f01a626657278
Matt Carroll <me@mattcarroll.name>
2018-05-30 21:30:38 -0700

    1. Static Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -24,14 +28,167 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      backgroundColor: Colors.black,
       body: new Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          new Text(
-            'Get Started!',
+          // Room for status bar
+          new Container(
+            width: double.infinity,
+            height: 20.0,
+          ),
+
+          // Cards
+          new Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: new Card(),
+            ),
+          ),
+
+          // Bottom Bar
+          new Container(
+            width: double.infinity,
+            height: 50.0,
+            color: Colors.grey,
           ),
         ],
       ),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        // Background
+        new ClipRRect(
+          borderRadius: new BorderRadius.circular(10.0),
+          child: new Image.asset(
+            'assets/van_on_beach.jpg',
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        // Content
+        new Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 30.0, left: 20.0, right: 20.0),
+              child: new Text(
+                '10th Street'.toUpperCase(),
+                style: new TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                  fontFamily: 'petita',
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                ),
+              ),
+            ),
+            new Expanded(child: new Container()),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Text(
+                  '2 - 3',
+                  style: new TextStyle(
+                    color: Colors.white,
+                    fontSize: 140.0,
+                    fontFamily: 'petita',
+                    letterSpacing: -5.0,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0, top: 30.0),
+                  child: new Text(
+                    'FT',
+                    style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 22.0,
+                      fontFamily: 'petita',
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Icon(
+                  Icons.wb_sunny,
+                  color: Colors.white,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: new Text(
+                    '65.1º',
+                    style: new TextStyle(
+                      color: Colors.white,
+                      fontFamily: 'petita',
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            new Expanded(child: new Container()),
+            Padding(
+              padding: const EdgeInsets.only(top: 50.0, bottom: 50.0),
+              child: new Container(
+                  decoration: new BoxDecoration(
+                    borderRadius: new BorderRadius.circular(30.0),
+                    border: new Border.all(
+                      color: Colors.white,
+                      width: 1.5,
+                    ),
+                    color: Colors.black.withOpacity(0.3),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        new Text(
+                          'Mostly Cloud',
+                          style: new TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'petita',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                        new Padding(
+                          padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                          child: new Icon(
+                            Icons.wb_cloudy,
+                            color: Colors.white,
+                          ),
+                        ),
+                        new Text(
+                          '11.2mph ENE',
+                          style: new TextStyle(
+                            color: Colors.white,
+                            fontFamily: 'petita',
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
